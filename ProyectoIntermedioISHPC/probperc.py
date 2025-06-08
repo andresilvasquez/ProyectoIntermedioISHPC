639244fdc082d6de113dc9198d00d3c21dd7fc31
JuanSeguraUNAL <jusegurag@unal.edu.co>
2025-06-08 18:58:49 -0500

    Se modifican los scripts de python para graficar

--- ProyectoIntermedioISHPC/probperc.py
+++ ProyectoIntermedioISHPC/probperc.py
@@ -8,16 +8,16 @@ sns.set_context("paper")
 
 def probabilidad(datos):
     try:
-        return np.mean(datos[:,0])
+        return np.mean(datos[:,0]), np.std(datos[:,0])
     except:
-        return np.nan
+        return np.nan, np.nan
 
 def tamanomax(datos):
     try:
         datos = datos[datos[:, 1] > 0, 1]
-        return np.mean(datos)
+        return np.mean(datos), np.std(datos)
     except:
-        return np.nan
+        return np.nan, np.nan
     
 def sumatoria(datos, columna):
     try:
@@ -36,16 +36,19 @@ def main():
     fig1, ax1 = plt.subplots(figsize=(10,5))
     fig2, ax2 = plt.subplots(figsize=(10,5))
     fig3, ax3 = plt.subplots(figsize=(10,5))
+    fig4, ax4 = plt.subplots(figsize=(10,5))
 
     for j, l in enumerate(L):
         prob = np.zeros(len(P))
+        desv = np.zeros(len(P))
         tam = np.zeros(len(P))
+        desvtam = np.zeros(len(P))
         for i, p in enumerate(P):
             datos = np.loadtxt(f"./resultados/datos_{l}_{p}_O3.txt")
-            prob[i] = probabilidad(datos)
-            tam[i] = tamanomax(datos)
-        ax1.plot(P, prob, label=f"L = {l}")
-        ax2.plot(P, tam / (l * l), label=f"L = {l}")
+            prob[i], desv[i] = probabilidad(datos)
+            tam[i], desvtam[i] = tamanomax(datos)
+        ax1.errorbar(P, prob, yerr=desv, label=f"L = {l}")
+        ax2.errorbar(P, tam / (l * l), yerr=desvtam / (l * l),label=f"L = {l}")
 
     for o in OPT:
         WT = np.zeros(len(L))
@@ -63,7 +66,25 @@ def main():
         ax3.plot(L, WT, label=f"Wall time - {o}")
         ax3.plot(L, CT, label=f"CPU time - {o}")
 
+    for o in OPT:
+        WT = np.zeros(len(L))
+        CT = np.zeros(len(L))
+        desvWT = np.zeros(len(L))
+        desvCT = np.zeros(len(L))
 
+        for j, l in enumerate(L):
+            wt = np.zeros(len(P))
+            ct = np.zeros(len(P))
+            for i, p in enumerate(P):
+                datos = np.loadtxt(f"./resultados/datos_{l}_{p}_{o}.txt")
+                wt[i] = sumatoria(datos, 2)
+                ct[i] = sumatoria(datos, 3)
+            WT[j] = np.mean(wt)
+            desvWT = np.std(wt)
+            CT[j] = np.mean(ct)
+            desvCT = np.std(ct)
+        ax4.errorbar(L, WT, yerr= desvWT,label=f"Wall time - {o}")
+        ax4.errorbar(L, CT, yerr= desvCT,label=f"CPU time - {o}")
 
     ax1.set_xlabel(r"Probabilidad de ocupacion $p$")
     ax1.set_ylabel(r"Probabilidad de cluster percolante $P(p,L)$")
@@ -84,11 +105,22 @@ def main():
     ax3.set_xlabel(r"Tamano del lado de la malla")
     ax3.set_ylabel(r"Tiempo")
     ax3.set_title("Tiempo de computo en funcion del tamano del sistema" )
+    ax3.set_xscale("log")
     ax3.set_yscale("log")
     ax3.legend()
     ax3.grid(True)
     fig3.tight_layout()
     fig3.savefig("Tiempos.pdf")
+
+    ax4.set_xlabel(r"Tamano del lado de la malla")
+    ax4.set_ylabel(r"Tiempo")
+    ax4.set_title("Tiempo de computo en funcion del tamano del sistema" )
+    ax4.set_xscale("log")
+    ax4.set_yscale("log")
+    ax4.legend()
+    ax4.grid(True)
+    fig4.tight_layout()
+    fig4.savefig("Tiempos(errorbar).pdf")
 
     plt.close("all")
     
